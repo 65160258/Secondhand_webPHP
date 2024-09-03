27d8f1065388f6bb5b479de0c21ec8fd08007834
65160258 <156194536+65160258@users.noreply.github.com>
2024-09-03 22:43:56 +0700

    Delete Secondhand_web_withBootstrapCSS directory

--- Secondhand_web_withBootstrapCSS/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function calculate_total($cart) {
-    $total = 0;
-    foreach ($cart as $product) {
-        $total += $product['price'] * $product['quantity'];
-    }
-    return $total;
-}
-?>
--- Secondhand_web_withBootstrapCSS/login_register/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // ลบข้อมูลเซสชันทั้งหมด
-session_destroy(); // ทำลายเซสชัน
-
-header("Location: ../index.php"); // เปลี่ยนเส้นทางไปยังหน้า index.php
-exit();
-?>
